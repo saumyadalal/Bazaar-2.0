69cd3e63d26ab119621318fd243bac0df2f5c241
Saumya Dalal <saumya@cmu.edu>
2014-04-19 18:28:11 -0400

    Added the marketplace table view controller to the storyboard

---
+++ Bazaar/BZRBazaarMarketplaceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BZRBazaarMarketplaceTableViewController.h
+//  Bazaar
+//
+//  Created by Saumya Dalal on 4/19/14.
+//  Copyright (c) 2014 cmu.barter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BZRBazaarMarketplaceTableViewController : UITableViewController
+
+@end
