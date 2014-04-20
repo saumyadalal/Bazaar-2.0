e54f192f06d543800e79b4b634b9fe7ba8e9ff98
Saumya Dalal <saumya@cmu.edu>
2014-04-20 15:43:15 -0400

    Added the item view, trade view and a view for the login screen (NOTE: login not setup to display first)

---
+++ Bazaar/BZRItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  BZRItemViewController.h
+//  Bazaar
+//
+//  Created by Saumya Dalal on 4/20/14.
+//  Copyright (c) 2014 cmu.barter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BZRItemViewController : UIViewController
+
+@end
---
+++ Bazaar/BZRNewLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  BZRNewLoginViewController.h
+//  Bazaar
+//
+//  Created by Saumya Dalal on 4/20/14.
+//  Copyright (c) 2014 cmu.barter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BZRNewLoginViewController : UIViewController
+- (IBAction)loginUser:(id)sender;
+
+
+@end
