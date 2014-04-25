00f06d442aa05cc5c91edd205f5a82446b05f573
nshikha <shikhara.nalla@gmail.com>
2014-04-25 19:21:27 -0400

    gitignore

---
+++ Bazaar/BZRUserMarketPlaceViewController.h
@@ -0,0 +1,13 @@
+//
+//  BZRUserMarketPlaceViewController.h
+//  Bazaar
+//
+//  Created by Shikhara Nalla on 4/25/14.
+//  Copyright (c) 2014 cmu.barter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BZRUserMarketPlaceViewController : UICollectionViewController
+
+@end
---
+++ Bazaar/BZRUserMarketPlaceViewController.m
@@ -0,0 +1,38 @@
+//
+//  BZRUserMarketPlaceViewController.m
+//  Bazaar
+//
+//  Created by Shikhara Nalla on 4/25/14.
+//  Copyright (c) 2014 cmu.barter. All rights reserved.
+//
+
+#import "BZRUserMarketPlaceViewController.h"
+
+@interface BZRUserMarketPlaceViewController ()
+
+@end
+
+@implementation BZRUserMarketPlaceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
