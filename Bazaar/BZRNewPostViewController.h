8185066e8ba0a194d4806768630b81e613c8b51f
Meghan Chandarana <mchandar@ww109-c8.ww.andrew.cmu.edu>
2014-05-05 22:10:06 -0400

    new post fixed, have to fill in all the fields before you can post an item

--- Bazaar/BZRNewPostViewController.h
+++ Bazaar/BZRNewPostViewController.h
@@ -18,5 +18,6 @@
 @property (strong, nonatomic) IBOutlet UITextField *category;
 - (IBAction)resign:(id)sender;
 @property (strong, nonatomic) IBOutlet UIImageView *addObject;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *postButton;
 
 @end
