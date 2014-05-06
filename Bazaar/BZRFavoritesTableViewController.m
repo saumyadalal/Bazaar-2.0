1dbb49294ca3c675ec8099b31dfb35876a69b0a3
Meghan Chandarana <mchandar@ww109-c8.ww.andrew.cmu.edu>
2014-05-05 22:50:15 -0400

    connected detail view to favorites

--- Bazaar/BZRFavoritesTableViewController.m
+++ Bazaar/BZRFavoritesTableViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BZRFavoritesTableViewController.h"
+#import "BZRDetailViewController.h"
 
 @interface BZRFavoritesTableViewController ()
 @property (nonatomic, strong) NSArray *items;
@@ -87,12 +88,15 @@ static NSString * const cellIdentifier = @"favoriteItemCell";
 }
 
 //instantiate detail view controller here since the segue in storyboard doesn't seem to work
-//- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-//    BZRItemViewController* detailView = [[UIStoryboard storyboardWithName:@"Storyboard" bundle:nil]
-//                                         instantiateViewControllerWithIdentifier:@"itemViewController"];
-//    detailView.item = [self.favoriteArray objectAtIndex:indexPath.row];
-//    [self.navigationController pushViewController:detailView animated:YES];
-//}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    BZRDetailViewController* detailView = [[UIStoryboard storyboardWithName:@"Storyboard" bundle:nil]
+                                         instantiateViewControllerWithIdentifier:@"detailViewController"];
+    detailView.items = self.favoriteArray;
+    detailView.currentIndexPath = indexPath;
+    detailView.inSelectionMode = NO;
+//    [detailView setDelegate:self];
+    [self.navigationController pushViewController:detailView animated:YES];
+}
 
 
 @end
